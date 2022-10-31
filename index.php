fb7fb513b91e554befdca46385e613a63d9e3808
Adriano Campos <adrianohcampos@gmail.com>
2022-10-31 19:44:52 -0300

    update

--- index.php
+++ index.php
@@ -6,9 +6,13 @@ use AdrianoHCampos\BNCC\BNCC;
 
 $bncc = new BNCC;
 
-$ensinoMedio = $bncc->buscaDados('fundamental', [1], [1]);
+// $ensinoInfantil = $bncc->buscaDados('infantil', [1, 2, 3], [1, 2, 3, 4, 5]);
+
+// $ensinoFundamental = $bncc->buscaDados('fundamental', [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+// $ensinoMedio = $bncc->buscaDados('medio', [1, 2, 3], [36, 37, 38, 39, 40]);
 
 echo '<pre>';
-print_r($ensinoMedio);
+print_r($ensinoFundamental);
 echo '</pre>';
 exit;
--- src/BNCC.php
+++ src/BNCC.php
@@ -4,7 +4,7 @@ namespace AdrianoHCampos\BNCC;
 
 class BNCC
 {
-
+    public $ensino, $idAnos;
     /**
      * URL Base da BNCC API
      *
@@ -47,7 +47,8 @@ class BNCC
      */
     public function buscaDados(string $ensino, array $idAnos, array $idCampos)
     {
-
+        $this->ensino = $ensino;
+        $this->idAnos = $idAnos;
         switch ($ensino) {
             case 'infantil':
                 $rota = 'extrairinfantil';
@@ -55,7 +56,6 @@ class BNCC
                     'idAnos' => $idAnos,
                     'idComponentes' => $idCampos
                 ];
-
                 break;
             case 'fundamental':
                 $rota = 'extrairfundamental';
@@ -63,7 +63,6 @@ class BNCC
                     'idAnos' => $idAnos,
                     'idComponentes' => $idCampos
                 ];
-
                 break;
             case 'medio':
                 $rota = 'extrair-medio';
@@ -71,7 +70,6 @@ class BNCC
                     'idAnos' => $idAnos,
                     'idAreas' => $idCampos
                 ];
-
                 break;
             default:
                 $rota = false;
@@ -86,18 +84,26 @@ class BNCC
 
         $path = __DIR__ . '/bncc' . $rota . time() . '.xlsx';
 
-        $client = new \GuzzleHttp\Client();
-        $resource = \GuzzleHttp\Psr7\Utils::tryFopen($path, 'w');
-        $response = $client->request('POST', $urlExtracao, [
-            'form_params' => $consulta,
-            'sink' => $resource
-        ]);
+        try {
+            $client = new \GuzzleHttp\Client();
+            $resource = \GuzzleHttp\Psr7\Utils::tryFopen($path, 'w');
+            $response = $client->request('POST', $urlExtracao, [
+                'form_params' => $consulta,
+                'sink' => $resource
+            ]);
+        } catch (\RuntimeException $e) {
+            unlink($path);
+            return $e->getMessage();
+            die;
+        }
 
         if ($response->getStatusCode() == 200) {
             $contentType = explode(';', $response->getHeaderLine('content-type'))[0];
             if ($contentType == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
-                $this->extrairDados($path);
+                return $this->extrairDados($path);
             }
+        } else {
+            unlink($path);
         }
 
         return false;
@@ -107,29 +113,125 @@ class BNCC
     {
         $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
         $reader->setReadDataOnly(true);
-        // $reader->setLoadSheetsOnly(["Arte", "Portugues"]);
         $spreadsheet = $reader->load($arquivo);
 
         $sheet_count = $spreadsheet->getSheetCount();
 
+        $res = [];
         for ($i = 0; $i < $sheet_count; $i++) {
+
             $sheet = $spreadsheet->getSheet($i);
             $title = $sheet->getTitle();
-            if (!str_contains($title, 'Comp')) {
-                echo '<pre>';
-                print_r($sheet->getTitle());
-                echo '</pre>';
+
+            if (
+                !str_contains($title, 'Com')
+                && !str_contains($title, 'Cam')
+                && !str_contains($title, 'Direitos')
+            ) {
+
+                $array = [];
+                $x = 0;
+
+                foreach ($sheet->toArray() as $value) {
+
+                    ++$x;
+                    if ($x <= 3) {
+                        continue;
+                    }
+                    if ($value[0] == '') {
+                        continue;
+                    }
+
+                    if ($this->ensino == 'infantil') {
+                        $data = $this->infantil($value);
+                    } else if ($this->ensino == 'fundamental') {
+                        $data = $this->fundamental($value);
+                    } else if ($this->ensino == 'medio') {
+                        $data = $this->medio($value);
+                    } else {
+                        $data = false;
+                    }
+
+                    if ($data) {
+                        $array[] = $data;
+                    }
+                }
+
+                $res[] = [
+                    'conteudo' => $title,
+                    'lista' => $array
+                ];
             }
-            // processa os dados da planilh
         }
 
-        exit;
-
-        echo '<pre>';
-        print_r($spreadsheet);
-        echo '</pre>';
-        exit;
-
         unlink($arquivo);
+
+        return $res;
+    }
+
+    public function infantil($value)
+    {
+        $codigo = $this->extraiCodigo($value[2]);
+        $ano = $value[1];
+        $habilidades = $value[2];
+
+        if ($codigo == null) {
+            return false;
+        }
+        return [
+            'ensino' => $this->ensino,
+            'codigo' => $codigo,
+            'ano' => $ano,
+            'unidadeTematica' => null,
+            'objetoConhecimento' => null,
+            'habilidades' => $habilidades,
+        ];
+    }
+
+    public function fundamental($value)
+    {
+        $codigo = $this->extraiCodigo($value[4]);
+        $ano = $value[1];
+        $unidadeTematica = $value[2];
+        $objetoConhecimento = $value[3];
+        $habilidades = $value[4];
+
+        if ($codigo == null) {
+            $codigo = $this->extraiCodigo($value[5]);
+            $ano = $value[1];
+            $unidadeTematica = $value[3];
+            $objetoConhecimento = $value[4];
+            $habilidades = $value[5];
+        }
+
+        return [
+            'ensino' => $this->ensino,
+            'codigo' => $codigo,
+            'ano' => $ano,
+            'unidadeTematica' => $unidadeTematica,
+            'objetoConhecimento' => $objetoConhecimento,
+            'habilidades' => $habilidades,
+        ];
+    }
+
+    public function medio($value)
+    {
+        $codigo = $value[1];
+
+        return [
+            'ensino' => $this->ensino,
+            'codigo' => $codigo,
+            'ano' => $value[0],
+            'unidadeTematica' => null,
+            'objetoConhecimento' => null,
+            'habilidades' => $value[2],
+        ];
+    }
+
+    public function extraiCodigo($string)
+    {
+        // extrai o codigo da habilidade
+        preg_match('#\((.*?)\)#', $string, $match);
+        return isset($match[1]) ? $match[1] : null;
     }
 }
